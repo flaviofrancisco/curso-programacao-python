7b63131bcb82d67e12d58c11a59e5215172d657a
flaviofrancisco <flaviofrancis@msn.com>
2021-06-20 11:52:42 +0200

    Bug fix

--- exercicios/03/cpf.py
+++ exercicios/03/cpf.py
@@ -7,6 +7,6 @@ class Cpf(DocumentoFiscal):
 
     def calcula_digito_verificador(self, cnpj, digito=1):
         pesos = self.__NUM_DV1 if digito == 1 else self.__NUM_DV2
-        resultado = (sum(int(digito) * peso for digito,peso in zip(cnpj, pesos))) % 11
+        resultado = (sum(int(digito) * peso for digito,peso in zip(cnpj, pesos)))
         resultado = (resultado * 10) % 11
         return 0 if resultado == 10 else resultado
